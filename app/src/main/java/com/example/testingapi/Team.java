23314db941b408d99a214ff015963573ff44be1f
full name <email address>
2025-04-24 13:05:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testingapi/Team.java
@@ -0,0 +1,18 @@
+package com.example.testingapi;
+
+import com.google.gson.annotations.SerializedName;
+
+public  class Team {
+
+    @SerializedName("strTeam")
+    public String strTeam;
+
+    @SerializedName("strStadium")
+    public String strStadium;
+
+    @SerializedName("strBadge")
+    public String strBadge;
+
+
+
+}
---
+++ app/src/main/java/com/example/testingapi/TeamResponse.java
@@ -0,0 +1,10 @@
+package com.example.testingapi;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class TeamResponse {
+    @SerializedName("teams")
+    public List<Team> teams;
+}
